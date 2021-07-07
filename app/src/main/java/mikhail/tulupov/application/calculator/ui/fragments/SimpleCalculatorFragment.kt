3f5a9616513be9aeecc92e9a4c8dc5a9bcaebdd9
Mikhail <kd.maikl-201@yandex.ru>
2021-07-07 11:09:15 +0700

    Create fragment

---
+++ app/src/main/java/mikhail/tulupov/application/calculator/ui/fragments/SimpleCalculatorFragment.kt
@@ -0,0 +1,6 @@
+package mikhail.tulupov.application.calculator.ui.fragments
+
+import androidx.fragment.app.Fragment
+import mikhail.tulupov.application.calculator.R
+
+class SimpleCalculatorFragment : Fragment(R.layout.fragment_simple_calculator)
