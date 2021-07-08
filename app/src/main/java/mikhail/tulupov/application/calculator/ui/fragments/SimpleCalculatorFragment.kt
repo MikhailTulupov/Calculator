135a28e46e413732e8d47a87218d7060b8eff8ab
Mikhail <kd.maikl-201@yandex.ru>
2021-07-08 09:22:53 +0700

    Add override fun onCreateView and init binding variable

--- app/src/main/java/mikhail/tulupov/application/calculator/ui/fragments/SimpleCalculatorFragment.kt
+++ app/src/main/java/mikhail/tulupov/application/calculator/ui/fragments/SimpleCalculatorFragment.kt
@@ -1,6 +1,35 @@
 package mikhail.tulupov.application.calculator.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import mikhail.tulupov.application.calculator.R
+import mikhail.tulupov.application.calculator.databinding.FragmentSimpleCalculatorBinding
 
-class SimpleCalculatorFragment : Fragment(R.layout.fragment_simple_calculator)
+class SimpleCalculatorFragment : Fragment() {
+
+    private lateinit var binding: FragmentSimpleCalculatorBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding =
+            DataBindingUtil
+                .inflate(
+                    inflater,
+                    R.layout.fragment_simple_calculator,
+                    container,
+                    false
+                )
+
+
+
+        return binding.root
+    }
+}
