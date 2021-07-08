f7896edec9aed2a2ef518bafbe48269e5e212904
Mikhail <kd.maikl-201@yandex.ru>
2021-07-08 10:46:51 +0700

    Add numBtnMap add for each entry listener and addNumber function
    Initialization numBtnMap. Adding for each entry listener. Create addNumber function which return number which equal key map

--- app/src/main/java/mikhail/tulupov/application/calculator/ui/fragments/SimpleCalculatorFragment.kt
+++ app/src/main/java/mikhail/tulupov/application/calculator/ui/fragments/SimpleCalculatorFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import mikhail.tulupov.application.calculator.R
@@ -12,6 +14,8 @@ import mikhail.tulupov.application.calculator.databinding.FragmentSimpleCalculat
 class SimpleCalculatorFragment : Fragment() {
 
     private lateinit var binding: FragmentSimpleCalculatorBinding
+    private lateinit var numBtnMap: Map<String, ImageButton>
+    private lateinit var numbersText: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,8 +32,41 @@ class SimpleCalculatorFragment : Fragment() {
                     false
                 )
 
+        numbersText = binding.etNumber
 
+        numBtnMap = mapOf(
+            "zero" to binding.ibNumberZero,
+            "one" to binding.ibNumberOne,
+            "two" to binding.ibNumberTwo,
+            "three" to binding.ibNumberThree,
+            "four" to binding.ibNumberFour,
+            "five" to binding.ibNumberFive,
+            "six" to binding.ibNumberSix,
+            "seven" to binding.ibNumberSeven,
+            "eight" to binding.ibNumberEight,
+            "nine" to binding.ibNumberNine
+        )
+
+        for ((number, view) in numBtnMap) {
+            view.setOnClickListener {
+                numbersText.text = numbersText.text.append(addNumber(number))
+            }
+        }
 
         return binding.root
+    }
+
+    //method return equal number to key
+    private fun addNumber(number: String): Char = when (number) {
+        "zero" -> '0'
+        "one" -> '1'
+        "two" -> '2'
+        "three" -> '3'
+        "four" -> '4'
+        "five" -> '5'
+        "six" -> '6'
+        "seven" -> '7'
+        "eight" -> '8'
+        else -> '9'
     }
 }
