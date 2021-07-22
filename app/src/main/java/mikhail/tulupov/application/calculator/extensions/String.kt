c28828cf04399ef8ab586885bdce498ddb6001bc
Mikhail <kd.maikl-201@yandex.ru>
2021-07-22 12:37:36 +0700

    Add new regex

--- app/src/main/java/mikhail/tulupov/application/calculator/extensions/String.kt
+++ app/src/main/java/mikhail/tulupov/application/calculator/extensions/String.kt
@@ -6,20 +6,22 @@ import java.util.regex.Pattern
 import kotlin.collections.ArrayList
 
 fun String.toTokens(expression: String): ArrayList<String> {
-        val pattern: Pattern =
-            Pattern.compile("((\\d+[.]\\d+)|(\\d+[.])|([.]\\d+)|(\\d+)|([+*-/])|([()]))")
-        val matcher: Matcher = pattern.matcher(expression)
-        val tokens = ArrayList<String>()
-        while (matcher.find()) {
-            when {
-                Objects.requireNonNull(matcher.group(1)).endsWith(".") -> tokens.add(
-                    matcher.group(1).toString() + "0"
-                )
-                Objects.requireNonNull(matcher.group(1))
-                    .startsWith(".") -> tokens.add("0" + matcher.group(1))
-                else -> tokens.add(matcher.group(1))
-            }
+    val regex =
+        "((\\d+[.]\\d+[E][+-]\\d+)|(\\d+[.]\\d+)|(\\d+[.])|([.]\\d+)|(\\d+)|([+*-/])|([()]))"
+    val pattern: Pattern =
+        Pattern.compile(regex)
+    val matcher: Matcher = pattern.matcher(expression)
+    val tokens = ArrayList<String>()
+    while (matcher.find()) {
+        when {
+            Objects.requireNonNull(matcher.group(1)).endsWith(".") -> tokens.add(
+                matcher.group(1).toString() + "0"
+            )
+            Objects.requireNonNull(matcher.group(1))
+                .startsWith(".") -> tokens.add("0" + matcher.group(1))
+            else -> tokens.add(matcher.group(1))
         }
-
-        return tokens
     }
+
+    return tokens
+}
